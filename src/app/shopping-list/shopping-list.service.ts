aded9f268a93dcffb469097fc73c0735ebf0dd17
rdrobisz92@gmail.com <rdrobisz92@gmail.com>
2019-03-13 19:02:58 +0100

    update event to rx

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,12 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -13,12 +14,12 @@ export class ShoppingListService {
 
     addIngredient(ingridient: Ingredient) {
         this.ingredients.push(ingridient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingridients: Ingredient[]) {
         this.ingredients.push(...ingridients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     getIngredient() {
