2dd724a9972c2c3fb1c5d5a32e373e3faa6b3b3c
rdrobisz92@gmail.com <rdrobisz92@gmail.com>
2019-03-18 23:16:34 +0100

    lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,21 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptorProviders } from './shared/interceptors/interceptors';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     SharedModule,
-    RecipesModule,
     AuthModule,
     ShoppingListModule,
     AppRoutingModule,
